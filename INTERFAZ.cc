555b136525f5592b61b8b87484d207ed723747cf
Adrian Queipo <aqp696@hotmail.com>
2012-05-23 05:18:38 -0600

    pruebas

--- INTERFAZ.cc
+++ INTERFAZ.cc
@@ -84,7 +84,7 @@ int t_connect(const t_direccion *tsap_destino, t_direccion *tsap_origen) {
     it_tx = KERNEL->CXs[indice_celda].TX.end();
     memcpy(&tsap_destino_aux,tsap_destino,sizeof(t_direccion));
     fprintf(stderr,"\nvamos a llamar a crear_pkt");
-    crear_pkt(*(it_tx->pkt),CR,&tsap_destino_aux,tsap_origen,NULL,0,indice_celda,0);
+    crear_pkt(it_tx->pkt,CR,&tsap_destino_aux,tsap_origen,NULL,0,indice_celda,0);
     fprintf(stderr,"\nEnviamos el pakete y nos bloqueamos");
     //enviamos el paquete y nos bloqueamos
     enviar_tpdu(tsap_destino->ip,it_tx->pkt,sizeof(tpdu));
