5882430cd6fc758dcaa9b530616f105c05427722
Adrian Queipo <aqp696@hotmail.com>
2012-05-29 09:02:09 -0600

    prueba

--- FUNCP.cc
+++ FUNCP.cc
@@ -122,7 +122,7 @@ int asign_conexion_CR(struct in_addr ip_remota,tpdu *puntero_pkt) {
     int i;
     int indice = EXNOTSAP;
     for (i = 0; i < NUM_MAX_CXs; i++) {
-        fprintf(stderr,"\nip_remota.s_addr: %d; KERNEL->CXs[%d].ip_local.s_addr: %d",ip_remota.s_addr,i,KERNEL->CXs[i].ip_local.s_addr);
+        //fprintf(stderr,"\nip_remota.s_addr: %s; KERNEL->CXs[%d].ip_local.s_addr: %s",inet_ntop(AF_INET, &ip_remota, ipcharbuf, 20),i,KERNEL->);
         
         if((ip_remota.s_addr == KERNEL->CXs[i].ip_local.s_addr)
                 &&(puntero_pkt->cabecera.puerto_dest == KERNEL->CXs[i].puerto_origen)){
